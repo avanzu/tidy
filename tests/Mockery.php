9f4731802c2ed25f854113481da2108acc1b833c
Marc Bach <mail@avanzu.de>
2018-04-19 22:03:17 +0200

    pulls mockery helpers to local scope in order to fix missing docblocks

--- tests/Mockery.php
+++ tests/Mockery.php
@@ -5,7 +5,58 @@
  * Date: 14.04.18
  */
 
-Mockery::globalHelpers();
+use Mockery\Matcher\AnyArgs;
+
+if (!function_exists("mock")) {
+    /**
+     * @param mixed ...$args
+     *
+     * @return \Mockery\MockInterface
+     * @see Mockery::mock()
+     */
+    function mock(...$args)
+    {
+        return call_user_func_array([Mockery::class, "mock"], $args);
+    }
+}
+
+if (!function_exists("spy")) {
+
+    /**
+     * @param mixed ...$args
+     *
+     * @return \Mockery\MockInterface
+     * @see Mockery::spy()
+     */
+    function spy(...$args)
+    {
+        return call_user_func_array([Mockery::class, "spy"], $args);
+    }
+}
+
+if (!function_exists("namedMock")) {
+    /**
+     * @param mixed ...$args
+     *
+     * @return \Mockery\MockInterface
+     * @see Mockery::namedMock()
+     */
+    function namedMock(...$args)
+    {
+        return call_user_func_array([Mockery::class, "namedMock"], $args);
+    }
+}
+
+if (!function_exists("anyArgs")) {
+    /**
+     * @return AnyArgs
+     */
+    function anyArgs()
+    {
+        return new AnyArgs();
+    }
+}
+
 
 
 if (!function_exists('tear_down')) {
