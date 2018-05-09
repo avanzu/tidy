d0757cdabb3c2a0d5342caee19beb613761ee0dc
Marc Bach <mail@avanzu.de>
2018-05-09 10:21:07 +0200

    improves test coverage

--- tests/Unit/Integration/Components/ComponentsTest.php
+++ tests/Unit/Integration/Components/ComponentsTest.php
@@ -8,30 +8,39 @@
 
 namespace Tidy\Tests\Unit\Integration\Components;
 
-use Tidy\Components\DataAccess\Comparison;
+use Tidy\Components\AccessControl\AccessControlBroker;
+use Tidy\Components\AccessControl\IClaimantProvider;
 use Tidy\Components\Events\IDispatcher;
 use Tidy\Components\Util\IStringUtilFactory;
 use Tidy\Integration\Components\Components;
 use Tidy\Tests\MockeryTestCase;
 
-class ComponentsTest extends MockeryTestCase
-{
-    public function testInstantiation()
-    {
+class ComponentsTest extends MockeryTestCase {
+    public function testInstantiation() {
         $components = new Components();
         $this->assertNotNull($components);
     }
 
-    public function testStringUtilFactory()
-    {
-        $components = new Components();
+    public function testInjection() {
+        $stringUtilFactory = mock(IStringUtilFactory::class);
+        $dispatcher        = mock(IDispatcher::class);
+        $broker            = mock(AccessControlBroker::class);
+        $components        = new Components($stringUtilFactory, $dispatcher, $broker);
+
+        $this->assertSame($stringUtilFactory, $components->stringUtilFactory());
+        $this->assertSame($dispatcher, $components->eventDispatcher());
+        $this->assertSame($broker, $components->accessControlBroker(mock(IClaimantProvider::class)));
+
+    }
+
+    public function testStringUtilFactory() {
+        $components        = new Components();
         $stringUtilFactory = $components->stringUtilFactory();
         $this->assertInstanceOf(IStringUtilFactory::class, $stringUtilFactory);
         $this->assertSame($stringUtilFactory, $components->stringUtilFactory());
     }
 
-    public function testEventDispatcher()
-    {
+    public function testEventDispatcher() {
         $components = new Components();
         $dispatcher = $components->eventDispatcher();
         $this->assertInstanceOf(IDispatcher::class, $dispatcher);
